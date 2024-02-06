59a6676c6e5df02e988db7e8fc7f0583154da26e
yeti-s <sel9371@gmail.com>
2024-02-06 23:55:00 +0900

    refactor: rename contents components to elements

--- contents/elements/Image.tsx
+++ contents/elements/Image.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-const GIF = ({src}) => <Layout><img src={src} alt="Otter dancing with a fish" /></Layout>;
+const Image = ({src}) => <Layout><ImageWrapper src={src}/></Layout>;
+const ImageWrapper = styled.img`
+    width: 100%;
+`
 const Layout = styled.div`
     width: 100%;
     display: flex;
     justify-content: center;
 `;
-export default GIF;
+export default Image;
---
+++ contents/elements/index.ts
@@ -0,0 +1,21 @@
+import Card from './Card';
+import CardContainer from './CardContainer';
+import ColorText from './ColorText';
+import Cols from './Cols';
+import Comment from './Comment';
+import Details from './Details';
+import Image from './Image';
+import Project from './Project';
+import TwoCols from './TwoCols';
+
+export {
+    Card,
+    CardContainer,
+    ColorText,
+    Cols,
+    Comment,
+    Details,
+    Image,
+    Project,
+    TwoCols
+};
